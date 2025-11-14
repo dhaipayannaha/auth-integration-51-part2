4f671200fa5077d38090601cee818508f9a8e5d6
DESKTOP-U096J61\dhaip <dhaipayannahadipya@gmail.com>
2025-11-15 02:03:27 +0600

    set up

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,29 @@ import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Root from './layouts/Root.jsx';
+import Home from './Components/Home/Home.jsx';
+import Login from './Components/Login/Login.jsx';
+import Register from './Register/Register.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: Root,
+    children:[
+      {
+        index: true,
+        Component: Home
+      },
+      {
+        path: 'login',
+        Component: Login
+      },
+      {
+        path: 'register',
+        Component: Register
+      }
+    ]
   },
 ]);
 
